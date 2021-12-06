5d6131a7a0cbbe0e64c486758ee4fa54ec739ff5
איציק איציק <esteban.jimenez99999@gmail.com>
2021-12-06 15:09:52 +0200

    Solve 704.binary_search

---
+++ algo1/704.binary_search.py
@@ -0,0 +1,32 @@
+from math import floor
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        print(f'{left=}')
+        print(f'{right=}')
+        mid = floor((right + left) / 2)
+        print(f'{mid=}')
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            left = mid + 1
+            continue
+        if nums[mid] > target:
+            right = mid - 1
+    return -1
+
+
+def main():
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 2
+    res = search(nums, target)
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
+
